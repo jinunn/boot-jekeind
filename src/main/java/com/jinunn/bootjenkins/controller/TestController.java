c6dd8b2c6fcba64e48c692257f3734196581b3a5
dun.jin@chiefclouds.com <dun.jin@chiefclouds.com>
2022-08-30 12:00:02 +0800

    first commit

---
+++ src/main/java/com/jinunn/bootjenkins/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.jinunn.bootjenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : JinDun
+ * @date : 2022/8/30 11:55
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/aaa")
+    public String getStr(){
+        return "hello git";
+    }
+}
---
+++ src/test/java/com/jinunn/bootjenkins/BootJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinunn.bootjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
